bdec8a16b20499548039e55e311f531e0ee427f6
s33743 <s33743@pjwstk.edu.pl>
2026-04-15 11:49:15 +0200

    początek klasy Pracownik-cd

--- PGO 5 - s33743/src/Pracownik.java
+++ PGO 5 - s33743/src/Pracownik.java
@@ -1,2 +1,46 @@
+import java.util.Objects;
+
 public class Pracownik {
+
+    private String idPracownika;
+    private String imie;
+    private String nazwisko;
+    private double stawkaBazowa;
+
+    public Pracownik(String idPracownika, String imie, String nazwisko, double stawkaBazowa) {
+        this.idPracownika = idPracownika;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.stawkaBazowa = stawkaBazowa;
+    }
+
+    public final String pobierzIdPracownika() { return idPracownika; }
+
+    public String getImie() { return imie; }
+
+    public String getNazwisko() { return nazwisko; }
+
+    public double getStawkaBazowa() { return stawkaBazowa; }
+
+    public double obliczKosztMiesieczny() { return stawkaBazowa; }
+
+    public String przedstawSie() {
+        return "Nazywam sie: " + imie + nazwisko;
+    }
+
+    @Override
+    public String toString() {
+        return "Pracownik{" + idPracownika + '\'' +
+                ", imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'' +
+                ", stawkaBazowa='" + stawkaBazowa + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) || getClass() != o.getClass()) return  false;
+    }
+
+
+
 }
