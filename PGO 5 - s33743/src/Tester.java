45b54bf59133e82b17b99e5433af52fb61cb9542
s33743 <s33743@pjwstk.edu.pl>
2026-04-20 21:22:33 +0200

    dodałem klase Tester

---
+++ PGO 5 - s33743/src/Tester.java
@@ -0,0 +1,56 @@
+public class Tester extends Pracownik {
+
+    private boolean czyAutomatyzujacy;
+    private int liczbaScenariuszy;
+
+    public Tester(String idPracownika,
+                  String imie,
+                  String nazwisko,
+                  double stawkaBazowa,
+                  boolean czyAutomatyzujacy,
+                  int liczbaScenariuszy) {
+        super(idPracownika, imie, nazwisko, stawkaBazowa);
+        this.czyAutomatyzujacy = czyAutomatyzujacy;
+        this.liczbaScenariuszy = liczbaScenariuszy;
+    }
+
+    public boolean getCzyAutomatyzujacy() {
+        return czyAutomatyzujacy;
+    }
+
+    public int getLiczbaScenariuszy() {
+        return liczbaScenariuszy;
+    }
+
+    @Override
+    public double obliczKosztMiesieczny() {
+        double koszt = getStawkaBazowa();
+        if (czyAutomatyzujacy == true) {
+            koszt += 100;
+        }
+        if (liczbaScenariuszy > 15) {
+            koszt += 100;
+        }
+        return koszt;
+    }
+
+    @Override
+    public String przedstawSie() {
+        return super.przedstawSie() + " i jestem testerem";
+    }
+
+    public void uruchomRaportTestow() {
+        String zautomatyzowanyStr;
+
+        if (czyAutomatyzujacy == true) {
+            zautomatyzowanyStr = "zautomatyzowany";
+        }
+        else {
+            zautomatyzowanyStr = "niezautomatyzowany";
+        }
+        System.out.println("Pracownik " + pobierzIdPracownika() + ": " +
+                getLiczbaScenariuszy() +
+                " scenariuszy, " + zautomatyzowanyStr
+        );
+    }
+}
